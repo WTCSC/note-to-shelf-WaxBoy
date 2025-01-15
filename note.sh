16c17ff96a8ecc29c9b7811f61df0dcd588688cc
mcbees <mcbeesteelix83@gmail.com>
2025-01-15 11:00:57 -0700

    Almost done with note.sh

--- note.sh
+++ note.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-notes="./notes.txt"
+notes="notes.txt"
 
 add_n() {
-    if ! [[ -f notes ]]; then
-        echo "No notes file here!"
-        exit 3
-    else
-        local note="$*"
-        note >> notes
-    fi
-
+        if [[ -n "$1" ]]; then
+            local timestamp=$(date +"%Y-%m-%d, %H:%M:%S")
+            echo "[$timestamp]  $*" >> $notes
+        else
+            echo "You must type a note."
+        fi
+}
 #search_n() {}
 
 #create list function
 list_n() {
     #if no notes or no notes file, give error, else list notes.
     if [[ -s $notes ]]; then
-        echo "You have at least 1 line of notes:"
-        cat $notes
+        
+        echo "You've got notes!"
+        cat $(tail -n +2 $notes)
         echo
     else
         echo "You poor soul. There are no notes here:" >&2
@@ -26,12 +26,65 @@ list_n() {
     fi
 }
 
+search_n() {
+    if [[ "$1" == 'searchcs' ]]; then
+        shift
+        search="$(grep "$*" "$notes")" #case sensitive search through notes
+    else
+        shift
+        search="$(grep -i "$*" "$notes")" #case insensitive search
+    fi
+    
+    if [[ -n $search ]]; then
+        echo "$search"
+    else
+        echo "No matches!"
+    fi
+}
+        #Go through 4 stages to confirm that you really want to delete your notes.
+clear_n() {
+    read -p "Are you sure you want to clear all of your notes? (Type 'Y' or 'N'): " answer
+    if [[ "${answer,,}" == "y" ]]; then
+        echo "Notes deleted on: [$(date +"%Y-%m-%d,%H:%M:%S")]" > $notes
+        #read -p "I don't believe you. (Type 'Yes' to confirm): " answer2
+
+        if [[ "${answer2,,}" == "yes" ]]; then
+            read -p "Im not convinced. Are you certain? (Type 'Affirmative'): " answer3
+
+            if [[ "${answer3,,}" == "affirmative" ]]; then
+                read -p "To make sure you understand, type 'I accept that my notes will be gone forever. I am willing to proceed.': " answer4
+                
+                if [[ "${answer4,,}" == "i accept that my notes will be gone forever. i am willing to proceed." ]]; then
+                    clank
+
+                else
+                    echo "You are not worthy."
+                fi
+            else
+                echo "What's wrong?"
+            fi
+        else 
+            echo "Okay"
+        fi
+    else
+        echo "Good choice"
+    fi
+        
+
+}
+    #allow for commands: list, add, search, searchcs (case sensitive), & clear
 case "$1" in
     list)
         list_n
         ;;
     add)
-        shift 
+        shift       #shift to skip over "add"
         add_n "$*"
+        ;;
+    search|searchcs)    
+        search_n "$@"
+        ;;
+    clear)
+        clear_n  #clears entire notes after confirmation
         ;;
 esac
