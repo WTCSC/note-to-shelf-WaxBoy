02397e8df0080f7b31a9e6c420bff1042ff6079a
mcbees <mcbeesteelix83@gmail.com>
2025-01-16 09:47:28 -0700

    Finished note.sh

--- note.sh
+++ note.sh
@@ -8,6 +8,7 @@ add_n() {
             echo "[$timestamp]  $*" >> $notes
         else
             echo "You must type a note."
+            exit 1
         fi
 }
 #search_n() {}
@@ -16,10 +17,15 @@ add_n() {
 list_n() {
     #if no notes or no notes file, give error, else list notes.
     if [[ -s $notes ]]; then
-        
-        echo "You've got notes!"
-        cat $(tail -n +2 $notes)
+    
+        echo "You've got notes!"        #discludes the "Notes Deleted On:"" line
+        if [[ "$(head -n 1 $notes | cut -c1)" != '[' ]]; then
+            tail -n +2 $notes  
+        else 
+            cat $notes        
+        fi
         echo
+
     else
         echo "You poor soul. There are no notes here:" >&2
         exit 2
@@ -39,14 +45,14 @@ search_n() {
         echo "$search"
     else
         echo "No matches!"
+        exit 3
     fi
 }
         #Go through 4 stages to confirm that you really want to delete your notes.
 clear_n() {
     read -p "Are you sure you want to clear all of your notes? (Type 'Y' or 'N'): " answer
     if [[ "${answer,,}" == "y" ]]; then
-        echo "Notes deleted on: [$(date +"%Y-%m-%d,%H:%M:%S")]" > $notes
-        #read -p "I don't believe you. (Type 'Yes' to confirm): " answer2
+        read -p "I don't believe you. (Type 'Yes' to confirm): " answer2
 
         if [[ "${answer2,,}" == "yes" ]]; then
             read -p "Im not convinced. Are you certain? (Type 'Affirmative'): " answer3
@@ -55,19 +61,23 @@ clear_n() {
                 read -p "To make sure you understand, type 'I accept that my notes will be gone forever. I am willing to proceed.': " answer4
                 
                 if [[ "${answer4,,}" == "i accept that my notes will be gone forever. i am willing to proceed." ]]; then
-                    clank
+                    echo "Notes deleted on: [$(date +"%Y-%m-%d,%H:%M:%S")]" > $notes
 
                 else
                     echo "You are not worthy."
+                    exit 4
                 fi
             else
                 echo "What's wrong?"
+                exit 4
             fi
         else 
             echo "Okay"
+            exit 4
         fi
     else
         echo "Good choice"
+        exit 4
     fi
         
 
@@ -82,7 +92,7 @@ case "$1" in
         add_n "$*"
         ;;
     search|searchcs)    
-        search_n "$@"
+        search_n "$@"   # use @ in order to pass in each argument including search/searchs, since it wasn't shifted
         ;;
     clear)
         clear_n  #clears entire notes after confirmation
